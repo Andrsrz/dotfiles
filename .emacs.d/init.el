7f996c940e49b219f197b69202b0702219860d71
Andres Ruiz <andrsruiz@gmail.com>
2020-02-11 17:05:51 -0600

    Update emacs config
    Deleted web mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -69,14 +69,3 @@
 ;; Install themes
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'dracula t)
-
-;; multi-web-mode
-(add-to-list 'load-path "~/.emacs.d/multi-web-mode")
-(require 'multi-web-mode)
-(setq mweb-default-major-mode 'html-mode)
-(setq mweb-tags
-	'((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-	(js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-	(css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
-(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
-(multi-web-global-mode 1)
