047b006f1ec6c81ecb0a8e7bfde1e394276f0a7c
Andres Ruiz <andrsruiz@gmail.com>
2020-03-03 15:48:04 -0600

    Update scripts

--- .config/rofi/scripts/menu_apps.sh
+++ .config/rofi/scripts/menu_apps.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-## Author : Aditya Shakya (adi1090x)
-## Mail : adi1090x@gmail.com
-## browser : @adi1090x
-## music : @adi1090x
-
 rofi_command="rofi -theme themes/menu/apps.rasi"
 
 # Links
