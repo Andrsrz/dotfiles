c6e2aed02a5bfa6e8c652ef3c993e69e70356a96
Andres Ruiz <andrsruiz@gmail.com>
2020-03-06 12:28:50 -0600

    Add laptop configs

---
+++ .config/rofi/scripts/Lmenu_powermenu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rofi_command="rofi -theme themes/menu/Lpowermenu.rasi"
+uptime=$(uptime -p | sed -e 's/up //g')
+
+# Options
+shutdown="襤"
+reboot="ﰇ"
+
+# Variable passed to rofi
+options="$shutdown\n$reboot"
+
+chosen="$(echo -e "$options" | $rofi_command -p "$uptime" -dmenu -selected-row 2)"
+case $chosen in
+    $shutdown)
+        systemctl poweroff
+        ;;
+    $reboot)
+        systemctl reboot
+        ;;
+esac
+
