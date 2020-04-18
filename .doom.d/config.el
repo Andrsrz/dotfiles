5c9288b6f1adefcfd7d5cfda4e54b93f8469c5ee
Andres Ruiz <andrsruiz@gmail.com>
2020-04-17 19:07:20 -0500

    Update Emacs config
    Better TABS

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -49,7 +49,7 @@
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
 
-;; Multiple-cursor
+; START MULTIPLE-CURSOR CONFIG
 (require 'multiple-cursors)
 ;; When you have an active region that spans multiple lines, the following will
 ;; add a cursor to each line:
@@ -59,7 +59,56 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+; END MULTIPLE-CURSOR CONFIG
 
-;; Magit
+; START MAGIT CONFIG
 (setq magit-view-git-manual-method 'man)
 (global-set-key (kbd "C-x g") 'magit-status)
+; END MAGIT CONFIG
+
+; START TABS CONFIG
+;; Create a variable for our preferred tab width
+(setq custom-tab-width 4)
+
+;; Two callable functions for enabling/disabling tabs in Emacs
+(defun disable-tabs () (setq indent-tabs-mode nil))
+(defun enable-tabs  ()
+  (local-set-key (kbd "TAB") 'tab-to-tab-stop)
+  (setq indent-tabs-mode t)
+  (setq tab-width custom-tab-width))
+
+;; Hooks to Enable Tabs
+(add-hook 'prog-mode-hook 'enable-tabs)
+(add-hook 'c-mode-hook 'enable-tabs)
+(add-hook 'c++-mode-hook 'enable-tabs)
+(add-hook 'python-mode-hook 'enable-tabs)
+(add-hook 'js-mode-hook 'enable-tabs)
+(add-hook 'lua-mode-hook 'enable-tabs)
+(add-hook 'php-mode-hook 'enable-tabs)
+(add-hook 'html-mode-hook 'enable-tabs)
+(add-hook 'css-mode-hook 'enable-tabs)
+(add-hook 'org-mode-hook 'enable-tabs)
+;; Hooks to Disable Tabs
+(add-hook 'lisp-mode-hook 'disable-tabs)
+(add-hook 'emacs-lisp-mode-hook 'disable-tabs)
+
+;; Language-Specific Tweaks
+(setq-default python-indent-offset custom-tab-width) ;; Python
+(setq-default js-indent-level custom-tab-width)      ;; Javascript
+
+;; Making electric-indent behave sanely
+(setq-default electric-indent-inhibit t)
+
+;; Make the backspace properly erase the tab instead of
+;; removing 1 space at a time.
+(setq backward-delete-char-untabify-method 'hungry)
+
+;; Visualize tabs as a pipe character - "|"
+;; This will also show trailing characters as they are useful to spot.
+(setq whitespace-style '(face tabs tab-mark trailing))
+(custom-set-faces
+ '(whitespace-tab ((t (:foreground "#636363")))))
+(setq whitespace-display-mappings
+  '((tab-mark 9 [124 9] [92 9]))) ; 124 is the ascii ID for '\|'
+(global-whitespace-mode) ; Enable whitespace mode everywhere
+; END TABS CONFIG
